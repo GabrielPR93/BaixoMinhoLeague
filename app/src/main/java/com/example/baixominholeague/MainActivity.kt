f24a88029a73847050e192a256f396e30523ca33
Gabriel <Gabrielpr93@hotmail.com>
2023-04-26 16:29:05 +0200

    menu configurado

--- app/src/main/java/com/example/baixominholeague/MainActivity.kt
+++ app/src/main/java/com/example/baixominholeague/MainActivity.kt
@@ -3,7 +3,12 @@ package com.example.baixominholeague
 import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.example.baixominholeague.databinding.ActivityMainBinding
+import com.example.baixominholeague.ui.menu.CalendarioFragment
+import com.example.baixominholeague.ui.menu.ClasificacionFragment
+import com.example.baixominholeague.ui.menu.InicioFragment
+import com.example.baixominholeague.ui.menu.JugadoresFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,5 +20,25 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.bottomNavigation.setBackground(null);
+
+        binding.bottomNavigation.setOnNavigationItemSelectedListener { item ->
+
+            when(item.itemId){
+                R.id.home -> replaceFragment(InicioFragment())
+                R.id.Buscar -> replaceFragment(JugadoresFragment())
+                R.id.clasificacion -> replaceFragment(ClasificacionFragment())
+                R.id.calendario -> replaceFragment(CalendarioFragment())
+
+            }
+            true
+        }
+
+        }
+    private fun replaceFragment(fragment: Fragment){
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameContainer,fragment)
+        fragmentTransaction.commit()
     }
 }
