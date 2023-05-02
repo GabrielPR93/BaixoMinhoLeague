cf4ffa89c3a9cfd4d5404a9c40fb6df94ace0d5a
Gabriel <Gabrielpr93@hotmail.com>
2023-05-02 16:47:39 +0200

    Activity de login

---
+++ app/src/main/java/com/example/baixominholeague/ui/menu/login/AuthActivity.kt
@@ -0,0 +1,12 @@
+package com.example.baixominholeague.ui.menu.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.baixominholeague.R
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
