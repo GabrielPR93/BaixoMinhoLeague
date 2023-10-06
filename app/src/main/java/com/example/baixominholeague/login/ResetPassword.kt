f42c1dea20a3a262ff19ea5915951a82c1f3d81b
Gabriel <Gabrielpr93@hotmail.com>
2023-10-06 12:50:55 +0200

    implementacion del diseño final de resetPassword

--- app/src/main/java/com/example/baixominholeague/login/ResetPassword.kt
+++ app/src/main/java/com/example/baixominholeague/login/ResetPassword.kt
@@ -3,6 +3,7 @@ package com.example.baixominholeague.login
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Toast
 import com.example.baixominholeague.databinding.ActivityResetPasswordBinding
 import com.google.firebase.auth.FirebaseAuth
@@ -33,23 +34,45 @@ class ResetPassword : AppCompatActivity() {
 
     private fun resetPassword(){
         var correo = bindin.editTextResetCorreo.text.toString()
+        bindin.progresBarResetPassword.visibility = View.VISIBLE // Mostrar ProgressBar
+
         if(!correo.isNullOrEmpty()){
             auth.sendPasswordResetEmail(correo)
                 .addOnCompleteListener { task ->
                     if (task.isSuccessful) {
-                        Toast.makeText(this, "Revise su correo electrónico para restablecer su contraseña", Toast.LENGTH_LONG).show()
+                        Toast.makeText(this, "Revise su correo electrónico para restablecer su contraseña", Toast.LENGTH_SHORT).show()
+                        showSuccessful(correo)
                     } else {
                         val exception = task.exception
                         if (exception is FirebaseAuthInvalidUserException) {
-                            Toast.makeText(this, "No hay una cuenta registrada con este correo electrónico", Toast.LENGTH_LONG).show()
+                            Toast.makeText(this, "No hay una cuenta registrada con este correo electrónico", Toast.LENGTH_SHORT).show()
+                            bindin.progresBarResetPassword.visibility = View.INVISIBLE
                         } else {
-                            Toast.makeText(this, "Error al enviar el correo electrónico de restablecimiento de contraseña", Toast.LENGTH_LONG).show()
+                            Toast.makeText(this, "Error al enviar el correo electrónico de restablecimiento de contraseña", Toast.LENGTH_SHORT).show()
+                            bindin.progresBarResetPassword.visibility = View.INVISIBLE
                         }
                     }
+                    bindin.progresBarResetPassword.visibility = View.INVISIBLE // Ocultar ProgressBar al finalizar
                 }
-        }else{
-            Toast.makeText(this, "Inserte el correo electrónico para restablecer la contraseña", Toast.LENGTH_LONG).show()
+                .addOnFailureListener {
+                    bindin.progresBarResetPassword.visibility = View.INVISIBLE // Ocultar ProgressBar al fallar
+                }
+        } else {
+            Toast.makeText(this, "Inserte el correo electrónico para restablecer la contraseña", Toast.LENGTH_SHORT).show()
+            bindin.progresBarResetPassword.visibility = View.INVISIBLE // Ocultar ProgressBar si no hay correo electrónico
         }
-
     }
+
+
+    private fun showSuccessful(correo: String){
+        bindin.tvSuccessful.setText("Consulte la dirección de correo electrónico ${correo.uppercase()} para restablecer su contraseña.\n Si no le aparece revise su carpeta de spam. ")
+        bindin.tvSuccessful.visibility = View.VISIBLE
+        bindin.tvInstrucciones.visibility = View.GONE
+        bindin.editTextResetCorreo.visibility = View.GONE
+        bindin.editTextResetCorreo2.visibility = View.GONE
+        bindin.btnResetPassword.setText("Reenviar email")
+        bindin.progresBarResetPassword.visibility = View.INVISIBLE
+    }
+
 }
+
